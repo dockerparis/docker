4583b109e977f4888fadc316635c0e811b0bda7f
Ghislain Guiot <gt.ghislain@gmail.com>
2014-12-13 06:15:22 +0100

    added basic skeleton of ncurses

--- api/client/pot.go
+++ api/client/pot.go
@@ -1,8 +1,14 @@
 package client
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"os/exec"
+	"text/tabwriter"
 	"time"
+
+	"code.google.com/p/goncurses"
 )
 
 type Pot struct {
@@ -11,27 +17,27 @@ type Pot struct {
 
 // CommonLine contains information common to each printed line
 type CommonLine struct {
-	Id string		// PID or docker hash
-	Command string		// command name or CMD
-	Uptime string		// container uptime or process uptime
-	Status string		// container status or process state
-	CPU string		// % of CPU used (if container, sum of % of processes)
-	RAM string		// RAM used (if container, sum of RAM of processes)
+	Id      string // PID or docker hash
+	Command string // command name or CMD
+	Uptime  string // container uptime or process uptime
+	Status  string // container status or process state
+	CPU     string // % of CPU used (if container, sum of % of processes)
+	RAM     string // RAM used (if container, sum of RAM of processes)
 }
 
 // ContainerLine contains information specific to a docker container line
 type ContainerLine struct {
-	Name string		// docker container name
-	Image string		// docker container image
-	CommonLine		// same props as processes
+	Name       string // docker container name
+	Image      string // docker container image
+	CommonLine        // same props as processes
 }
 
 // ProcessLine contains information about a process
 type ProcessLine ContainerLine
 
-type Container struct {
-	container ContainerLine	// information about the container	
-	processes []ProcessLine	// information about the processes
+type Containe struct {
+	container ContainerLine // information about the container
+	processes []ProcessLine // information about the processes
 }
 
 // Returns the list of running containers as well as internal processes
@@ -42,10 +48,68 @@ func (pot *Pot) Snapshot() {
 	}
 }
 
-func (pot *Pot) Run() {
+type Container struct {
+	Cpu   string
+	Port  string
+	State string
+}
+
+var m = map[string]Container{
+	"pomme":   Container{"93%", "4242", "running"},
+	"poire":   Container{"43.7%", "9754", "killed"},
+	"haricot": Container{"72.5%", "3452", "sleeping"},
+}
+
+func test(stdscr *goncurses.Window, c chan goncurses.Key) {
 	for {
-		pot.Snapshot()
-		time.Sleep(1 * 1e9)
+		c <- stdscr.GetChar()
+	}
+}
+
+func key(k goncurses.Key) bool {
+	switch k {
+	case 'q':
+		return false
+	default:
+		return true
+	}
+
+}
+
+func (pot *Pot) Run() {
+	stdscr, err := goncurses.Init()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer goncurses.End()
+	k := make(chan goncurses.Key)
+	t := time.Tick(time.Second)
+
+	go test(stdscr, k)
+	for {
+		stdscr.Erase()
+		select {
+		case kk := <-k:
+			stdscr.Println("key pressed!")
+			if !key(kk) {
+				return
+			}
+		case <-t:
+			o, _ := exec.Command("uptime").Output()
+			stdscr.Printf("%s\n", o)
+
+			b := new(bytes.Buffer)
+			t := new(tabwriter.Writer)
+			t.Init(b, 0, 8, 1, '\t', tabwriter.AlignRight)
+			fmt.Fprintln(t, "a\tb\tc\td\t")
+			for key, val := range m {
+				fmt.Fprintf(t, "%s\t%s\t%s\t%s\n", key, val.Cpu, val.Port, val.State)
+			}
+			t.Flush()
+			stdscr.Printf("%s", b.Bytes())
+		}
+		stdscr.Refresh()
 	}
 }
 
